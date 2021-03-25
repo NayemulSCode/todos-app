ad7065cb0968f5cfc10b504f8b67a52f8b5f26b5
Nayemul <nayemulhasan2016@gmail.com>
2021-03-25 18:57:43 +0600

    delete todos

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ client.connect(err => {
       res.send(result.modifiedCount>0);
     })
   })
+  //delete todos
+  app.delete('/delete/:id',(req,res) =>{
+      collection.deleteOne({_id: ObjectId(req.params.id)})
+      .then(result =>{
+          res.send(result.deletedCount>0);
+      })
+  })
 });
 
 
